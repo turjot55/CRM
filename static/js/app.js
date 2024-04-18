e2a3f3688816acdeb0e3a69e13c2c5b4997c9a76
Turja Talukder <turjo_t@yahoo.com>
2024-04-18 11:34:50 +0600

    first commit

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+// Message/Notification timer
+
+var message_timeout = document.getElementById("message-time");
+
+setTimeout(function () {
+  message_timeout.style.display = "none";
+}, 5000);
---
+++ webapp/views.py
@@ -0,0 +1,131 @@
+from django.shortcuts import render, redirect
+from .forms import CrateUserForm, LoginForm, CreateRecordForm, UpdateRecordForm
+
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+from . models import Record
+from django.shortcuts import render
+from django.contrib import messages
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def home(request):
+    
+    # return HttpResponse("Hello World!")
+    return render(request, "webapp/index.html")
+
+# Register
+
+def register(request):
+    form = CrateUserForm()
+    if request.method == "POST":
+        
+        form = CrateUserForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            messages.success(request, "Account created successfully")
+            
+            return redirect('my-login')
+            
+    context = {'form': form}
+    
+    return render(request, "webapp/register.html", context = context) 
+
+# login in user
+
+def my_login(request):
+    form = LoginForm()
+    if request.method == 'POST':
+    
+       form = LoginForm(request, data=request.POST)
+       
+       if form.is_valid():
+           username = request.POST.get("username")
+           password = request.POST.get("password")
+           
+           user = authenticate(request, username=username, password=password)
+           
+           if user is not None: 
+               auth.login(request, user)
+               
+               return redirect('dashboard')
+           
+               messages.success(request, "Successfully log in")
+    context = {'form' : form}
+    return render(request, 'webapp/my-login.html', context=context)
+
+
+
+# Dashboard
+@login_required(login_url='my-login')
+def dashboard(request):
+    my_records = Record.objects.all()
+    context = {'records': my_records}
+    return render(request, 'webapp/dashboard.html', context=context)
+
+# create record
+@login_required(login_url='my-login')
+def create_record(request):
+    
+    form = CreateRecordForm() 
+    if request.method == "POST":
+        form = CreateRecordForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Successfully Created")
+            
+            return redirect("dashboard") 
+    
+    context = {"form" : form}
+    return render(request, 'webapp/create-record.html', context=context)
+
+
+# Update a record
+@login_required(login_url='my-login')
+def update_record(request, pk):
+    record = Record.objects.get(id=pk)
+    
+    form = UpdateRecordForm(instance=record)
+    
+    if request.method == "POST":
+        form = UpdateRecordForm(request.POST, instance=record)
+        
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Updated database")
+            
+            return redirect("dashboard")
+    context = {'form': form}
+    return render(request, "webapp/update-record.html", context=context)
+
+
+# read/ view a singular record 
+@login_required(login_url='my-login')
+def singular_record(request, pk):
+    all_records = Record.objects.get(id=pk)
+    context = {"record": all_records}
+    return render(request, "webapp/view-record.html", context=context)
+
+
+# Delete a record
+@login_required(login_url='my-login')
+def delete_record(request, pk):
+    record = Record.objects.get(id=pk)
+    record.delete()
+    messages.success(request, "Deleted Successfully")
+    return redirect("dashboard")
+
+
+# user logout
+
+def user_logout(request):
+    
+    auth.logout(request)
+    messages.success(request, "Log out success")
+    return redirect("my-login")
